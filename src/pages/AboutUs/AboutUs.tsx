5b683974226f187a17d36f6c4949f3e2dfd417bd
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2022-08-31 17:30:26 -0300

    feat: criando pagina aboutUs, estilanzdo backgrounds e textos

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,6 +1,39 @@
-export default function AboutUs() {
+import Header from "../../components/Header/Header";
+import { DivBackground, Main } from "./styles";
 
+export default function AboutUs() {
   return (
-    <h1>Page About us</h1>
-  )
+    <>
+      <Header />
+      <DivBackground>
+        <Main>
+          <div>
+            <h1> Quem somos </h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore
+              delectus nobis nihil odit corrupti soluta obcaecati nam vel dolor
+              ducimus. Laboriosam doloremque ea rem quisquam molestiae nisi.
+              Voluptate, et. Pariatur?
+              <br />
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore
+              delectus nobis nihil odit corrupti soluta obcaecati nam vel dolor
+              ducimus. Laboriosam doloremque ea rem quisquam molestiae nisi.
+              Voluptate, et. Pariatur?
+            </p>
+          </div>
+          <div>
+            <h2> O que Fazemos </h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore
+              delectus nobis nihil odit corrupti soluta obcaecati nam vel dolor
+              ducimus. Laboriosam doloremque ea rem quisquam molestiae nisi.
+              Voluptate, et. Pariatur?
+            </p>
+          </div>
+        </Main>
+        <footer>
+        </footer>
+      </DivBackground>
+    </>
+  );
 }
