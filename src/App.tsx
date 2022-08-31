4f6ca18534dacfd0fb6120715a71ac7bb4ea21ff
clayson <clayson420eufrasio@gmail.com>
2022-08-31 09:55:47 -0300

    feat: excluido tag header do app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import Header from './components/Header/Header';
 import AuthProvider from './contexts/authContext/AuthContext';
 import RouteMain from './routes/MainRouter';
 import Global from './styles/global';
 
 function App() {
   return (
-    /*
-    <AuthProvider>
-        <Header/>
-    </AuthProvider>
-  */
     <AuthProvider>
       <Global />
       <RouteMain />
