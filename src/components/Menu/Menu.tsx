5091b6aacbb3f5180293d6f81c3fccbeb8c27b37
patrickcordeiro <patrick.cordeiro30@gmail.com>
2022-09-05 13:58:24 -0300

    fix: Corrigindo botão logout mobile

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,37 +1,64 @@
 import { ContainerModal, DivLinks, DivMatriz, DivSocials } from "./styles";
-import {AiOutlineYoutube,AiOutlineFacebook,AiOutlineInstagram,AiOutlineWhatsApp} from "react-icons/ai";
-import {BiLogIn,BiSearchAlt,BiHome,BiDonateHeart,BiLogOut,} from "react-icons/bi";
+import {
+  AiOutlineYoutube,
+  AiOutlineFacebook,
+  AiOutlineInstagram,
+  AiOutlineWhatsApp,
+} from "react-icons/ai";
+import {
+  BiLogIn,
+  BiSearchAlt,
+  BiHome,
+  BiDonateHeart,
+  BiLogOut,
+} from "react-icons/bi";
 import { TbHeartHandshake } from "react-icons/tb";
 import { Link } from "react-router-dom";
 import { IoIosPeople } from "react-icons/io";
-import {  useContext } from "react";
+import { useContext } from "react";
 import { AuthContext } from "../../contexts/authContext/AuthContext";
 import { CgProfile } from "react-icons/cg";
 import ResetPage from "../AboutTeam/ResetPage";
 
 export default function Menu() {
-  const { isLogin,setIsModal } = useContext(AuthContext);
-  const handleOutSideClick = (e:any )=>{
-    if(e.target.id === 'modalMenu'){
-      setIsModal(false)
+  const { isLogin, setIsModal, logout } = useContext(AuthContext);
+  const handleOutSideClick = (e: any) => {
+    if (e.target.id === "modalMenu") {
+      setIsModal(false);
     }
-  }
+  };
   return (
-    <ContainerModal id='modalMenu' onClick={handleOutSideClick}>
+    <ContainerModal id="modalMenu" onClick={handleOutSideClick}>
       <DivLinks>
         <DivSocials>
           <h3>Nossas Redes Sociais</h3>
           <div>
-            <a target='_blank' rel="noopener noreferrer" href="https://youtube.com">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://youtube.com"
+            >
               <AiOutlineYoutube />
             </a>
-            <a target='_blank' rel="noopener noreferrer" href="https://facebook.com">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://facebook.com"
+            >
               <AiOutlineFacebook />
             </a>
-            <a target='_blank' rel="noopener noreferrer" href="https://instagram.com">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://instagram.com"
+            >
               <AiOutlineInstagram />
             </a>
-            <a target='_blank' rel="noopener noreferrer" href="https://whatsapp.com" >
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://whatsapp.com"
+            >
               <AiOutlineWhatsApp />
             </a>
           </div>
@@ -44,7 +71,7 @@ export default function Menu() {
         </DivMatriz>
 
         <div>
-          <Link to="/home" replace >
+          <Link to="/home" replace>
             <BiHome />
             Home
           </Link>
@@ -65,18 +92,26 @@ export default function Menu() {
             Apoie Agora
           </Link>
           {isLogin ? (
-           <>
-           <Link to="/usuario" replace > <CgProfile/>Perfil </Link>
-           <Link to="/home" replace > <BiLogOut/>Logout</Link>
-           </>
+            <>
+              <Link to="/usuario" replace>
+                {" "}
+                <CgProfile />
+                Perfil{" "}
+              </Link>
+              <Link to="/home" replace onClick={logout}>
+                {" "}
+                <BiLogOut />
+                Logout
+              </Link>
+            </>
           ) : (
-            <Link to="/login" replace >
+            <Link to="/login" replace>
               <BiLogIn /> Login
             </Link>
           )}
         </div>
       </DivLinks>
-      <ResetPage/>
+      <ResetPage />
     </ContainerModal>
   );
 }
