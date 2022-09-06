d1e6f47aa9899afba899e1f0b056ef3f7a29f275
clayson <clayson420eufrasio@gmail.com>
2022-09-06 07:58:59 -0300

    feat: excuido as imagens sem uso/ adicionado o texto sobre Clayson Roberto Eufrasio

--- src/components/AboutTeam/AboutTeam.tsx
+++ src/components/AboutTeam/AboutTeam.tsx
@@ -92,28 +92,12 @@ export default function AboutTeam() {
         </a>
         <br />
         <p className="cardDev">
-          "Olá a todos, eu sou Henrique, sou desenvolvedor front-end e estudante
-          de desenvolvimento full-stack na Kenzie Academy Brasil. Este Projeto
-          foi iniciado como um projeto de conclusão de módulo que tinha como o
-          principal conceito resolver um problema, estavamos em brainstorm
-          analisando qual problemas poderiamos resolver, quando o Patrick
-          colocou seu projeto na mesa, explicou que não havia um banco de dados
-          para pessoas em situação de rua e que sua ideia seria criar esta
-          database para que ongs e outros canais que tem acesso a estes
-          desabrigados pudessem se cadastrar em nosso site, e apartir dai
-          cadastrar os moradores de rua, colocando dados que permitessem mostrar
-          o quão qualificados eram, e se tinham intenção de voltar ao mercado de
-          trabalho, aceitamos a ideia de imediato, então expliquei que
-          poderiamos adicionar tambem algumas features, onde volutários pudessem
-          se cadastrar para alguns projetos que as ongs poderiam agendar em
-          nosso web site, e tambem uma area de pesquisa para que as pessoas
-          cadastradas tivessem acesso ao banco de dados dos desabrigados, para
-          que pudessem encontrar algum parente desaparecido que talvez estivesse
-          em situação de rua. então hoje estamos aprensentando nossa primeira
-          versão do website, estou muito orgulhoso do nosso trabalho, espero que
-          consigamos ajudar muitas pessoas a mudar de vida."
+          "Me chamo Clayson tenho 34 anos sou de Sorocaba-SP,  hoje moro em Florianópolis-SC há uns 10 anos, aqui foi onde conheci minha esposa e tivemos uma filha linda, anteriormente trabalhava como vendedor de alimentos e bebidas em um restaurante  noturno, devido a pandemia o estabelecimento fechou e fomos todos demitidos.
+          Hoje trago uma bagagem de Front-End extensa onde foi adquirida através da escola Kenzie Academy Brasil, assim como a possibilidade desse nosso primeiro projeto solo desde zero até a entrega final.
+          Onde me senti muito honrado por fazer parte de uma equipe centrada nos seus objetivos, conseguindo aumentar ainda mais meus conhecimentos."
+
         </p>
-       
+
       </p>
       <p>
         <a href="https://www.linkedin.com/in/emerson-gon%C3%A7alves-dos-santos-404a8093">
