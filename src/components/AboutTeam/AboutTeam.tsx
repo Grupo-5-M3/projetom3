a3628963ee08a298eb23af41795d9883f7d177fa
henriquepiresdev <henrique.piresbezerra1312@gmail.com>
2022-09-07 14:23:08 -0300

    fix: historia de developers

--- src/components/AboutTeam/AboutTeam.tsx
+++ src/components/AboutTeam/AboutTeam.tsx
@@ -36,26 +36,18 @@ export default function AboutTeam() {
         </a>
         <br />
         <p className="cardDev">
-          "Olá a todos, eu sou Henrique, sou desenvolvedor front-end e estudante
-          de desenvolvimento full-stack na Kenzie Academy Brasil. Este Projeto
-          foi iniciado como um projeto de conclusão de módulo que tinha como o
-          principal conceito resolver um problema, estavamos em brainstorm
-          analisando qual problemas poderiamos resolver, quando o Patrick
-          colocou seu projeto na mesa, explicou que não havia um banco de dados
-          para pessoas em situação de rua e que sua ideia seria criar esta
-          database para que ongs e outros canais que tem acesso a estes
-          desabrigados pudessem se cadastrar em nosso site, e apartir dai
-          cadastrar os moradores de rua, colocando dados que permitessem mostrar
-          o quão qualificados eram, e se tinham intenção de voltar ao mercado de
-          trabalho, aceitamos a ideia de imediato, então expliquei que
-          poderiamos adicionar tambem algumas features, onde volutários pudessem
-          se cadastrar para alguns projetos que as ongs poderiam agendar em
-          nosso web site, e tambem uma area de pesquisa para que as pessoas
-          cadastradas tivessem acesso ao banco de dados dos desabrigados, para
-          que pudessem encontrar algum parente desaparecido que talvez estivesse
-          em situação de rua. então hoje estamos aprensentando nossa primeira
-          versão do website, estou muito orgulhoso do nosso trabalho, espero que
-          consigamos ajudar muitas pessoas a mudar de vida."
+          Olá meu nome é Patrick, tenho 25 anos, moro em SP e sou aluno da
+          Kenzie Academy Brasil. Tive o privilégio de participar desse projeto
+          final de módulo onde tinhamos que criar uma aplicação em grupo de tema
+          livre. Sugeri esse tema ao grupo pois foi algo que me sensibilizou
+          após eu ter visto uma reportagem na tv há cerca de um ano atrás. A
+          idéia desse projeto é criar um banco de dados de pessoas em situação
+          de rua, e torná-los visíveis para a sociedade novamente. Onde outros
+          poderão ver sua história de vida e suas habilidades e que com isso
+          possamos conectá-los aqueles que querem ajudá-los. Fiquei muito feliz
+          com o resultado do projeto e com o apoio e dedicação de todo o grupo.
+          Espero que esse projeto possa servir de inspiração para outras
+          pessoas, e que isso possa ajudar de verdade aqueles que mais precisam.
         </p>
       </p>
       <p>
@@ -92,12 +84,17 @@ export default function AboutTeam() {
         </a>
         <br />
         <p className="cardDev">
-          "Me chamo Clayson tenho 34 anos sou de Sorocaba-SP,  hoje moro em Florianópolis-SC há uns 10 anos, aqui foi onde conheci minha esposa e tivemos uma filha linda, anteriormente trabalhava como vendedor de alimentos e bebidas em um restaurante  noturno, devido a pandemia o estabelecimento fechou e fomos todos demitidos.
-          Hoje trago uma bagagem de Front-End extensa onde foi adquirida através da escola Kenzie Academy Brasil, assim como a possibilidade desse nosso primeiro projeto solo desde zero até a entrega final.
-          Onde me senti muito honrado por fazer parte de uma equipe centrada nos seus objetivos, conseguindo aumentar ainda mais meus conhecimentos."
-
+          "Me chamo Clayson tenho 34 anos sou de Sorocaba-SP, hoje moro em
+          Florianópolis-SC há uns 10 anos, aqui foi onde conheci minha esposa e
+          tivemos uma filha linda, anteriormente trabalhava como vendedor de
+          alimentos e bebidas em um restaurante noturno, devido a pandemia o
+          estabelecimento fechou e fomos todos demitidos. Hoje trago uma bagagem
+          de Front-End extensa onde foi adquirida através da escola Kenzie
+          Academy Brasil, assim como a possibilidade desse nosso primeiro
+          projeto solo desde zero até a entrega final. Onde me senti muito
+          honrado por fazer parte de uma equipe centrada nos seus objetivos,
+          conseguindo aumentar ainda mais meus conhecimentos."
         </p>
-
       </p>
       <p>
         <a href="https://www.linkedin.com/in/emerson-gon%C3%A7alves-dos-santos-404a8093">
@@ -105,7 +102,20 @@ export default function AboutTeam() {
         </a>
         <br />
         <p className="cardDev">
-          "Me chamo Emerson Gonçalves dos Santos, sou natural de Mariliuz/PR, atualmente moro em Curitiba/PR, tenho 34 anos sou casado e tenho um filho de 5 anos. No momento trabalho como metalúrgico na empresa CNH Industrial, em março de 2022 comecei a estudar desenvolvimento fullstack na Kenzie Academy Brasil onde aprendi e continuo aprendendo muito sobre pragramacao web, a ideia desse projeto foi do nosso amigo e integrante da equipe Patrick Cordeiro, quando ele falou sobre esse assunto foi unânime a aprovação dos membros do time. Por se tratar de uma ação social fomos todos impactados pois olhamos para a realidade cruel do nosso cotidiano e praticamos a empatia. Sabemos que sozinhos não podemos mudar o mundo mas sabemos que podemos colaborar pra um amanhã melhor para essas pessoa, quando nos unimos e damos as mão por um bem em comum nos tornamos mais fortes para ajudar a mudar a realidade de muitas pessoas que vivem nas ruas."
+          "Me chamo Emerson Gonçalves dos Santos, sou natural de Mariliuz/PR,
+          atualmente moro em Curitiba/PR, tenho 34 anos sou casado e tenho um
+          filho de 5 anos. No momento trabalho como metalúrgico na empresa CNH
+          Industrial, em março de 2022 comecei a estudar desenvolvimento
+          fullstack na Kenzie Academy Brasil onde aprendi e continuo aprendendo
+          muito sobre pragramacao web, a ideia desse projeto foi do nosso amigo
+          e integrante da equipe Patrick Cordeiro, quando ele falou sobre esse
+          assunto foi unânime a aprovação dos membros do time. Por se tratar de
+          uma ação social fomos todos impactados pois olhamos para a realidade
+          cruel do nosso cotidiano e praticamos a empatia. Sabemos que sozinhos
+          não podemos mudar o mundo mas sabemos que podemos colaborar pra um
+          amanhã melhor para essas pessoa, quando nos unimos e damos as mão por
+          um bem em comum nos tornamos mais fortes para ajudar a mudar a
+          realidade de muitas pessoas que vivem nas ruas."
         </p>
       </p>
     </>
